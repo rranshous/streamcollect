2abc3784198259c6dc8b7fe130183f40eee08f51
Robby Ranshous <rranshous@gmail.com>
2011-08-27 16:53:54 -0400

    better handling

--- collector.py
+++ collector.py
@@ -18,13 +18,31 @@ class Transporter(asynchat.async_chat):
         self.collector = collector
         self.addr = addr
 
+        # register ourself
+        self.collector.transporters.append(self)
+
     def collect_incoming_data(self,data):
         log.debug('Transporter [%s]: Collecting data: %s' % (self.addr,
                                                              len(data)))
         # need to subclass this method
         self.handle_data(data)
 
+    def writtable(self):
+        # we can always take more data
+        return True
+
+    def readable(self):
+        # we don't care to return data
+        return False
+
+    def handle_data(self,data):
+        pass
+
     def handle_close(self):
+        self.close()
+
+    def close(self):
+        self.collector.transporters.remove(self)
         self.close()
 
 class Collector(asyncore.dispatcher):
@@ -44,4 +62,27 @@ class Collector(asyncore.dispatcher):
         conn, addr = self.accept()
         log.info('Collector [%s]: Handling accept' % addr)
         Transporter(self, conn, addr)
+
+    def close(self):
+        # close all connections
+        for transporter in self.transporters:
+            transporter.close()
+
+
+if __name__ == '__main__':
+    log.info('Starting')
+
+    # read the port
+    port = sys.argv[1]
+
+    log.info('Setting up collector: %s' % port)
+    collector = Collector(int(port))
+
+    log.debug('Starting loop')
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        log.info('Keyboard Stop')
+        collector.close()
+
 
