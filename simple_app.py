ec422e81ca1269efc49af44696999af15ebc3cf9
Robby Ranshous <rranshous@gmail.com>
2011-08-28 20:04:44 -0400

    ~works, for some reason my client isn't parsing the response correctly..

--- simple_app.py
+++ simple_app.py
@@ -2,5 +2,5 @@
 
 def application(environ, start_response):
     print 'app ran'
-    start_response()
-    return ""
+    start_response("200 OK",[('Content-type','text/html')])
+    return "test!"
